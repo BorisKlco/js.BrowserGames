581277f2de4db6ff5218d71f8194caab147c9d58
BorisKlco <klco@pm.me>
2023-09-04 20:48:15 +0200

    2 files modified

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import './style.css';
 import { setupCounter } from './counter.js';
+import { Stopwatch } from './001 - Stopwatch/stopwatch.js';
+
+const sw = new Stopwatch();
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -10,11 +13,33 @@ document.querySelector('#app').innerHTML = `
     <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
+      <p id="status">Status</p>
+      <p id="duration">  </p>
+      <button id="start" type="button">Start</button>
+      <button id="stop" type="button">Stop</button>
     </div>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
   </div>
 `;
+
+function start() {
+  document.querySelector('#status').innerHTML = `Starting`;
+  sw.start();
+}
+function stop() {
+  document.querySelector('#status').innerHTML = `Stopping`;
+  sw.stop();
+  setDuration(sw.duration);
+}
+
+function setDuration(time) {
+  document.querySelector('#duration').innerHTML = `${time}s`;
+}
+
+document.querySelector('#start').addEventListener('click', () => start());
+document.querySelector('#stop').addEventListener('click', () => stop());
+console.log(sw.duration);
 
 setupCounter(document.querySelector('#counter'));
