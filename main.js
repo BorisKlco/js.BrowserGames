6c90386cd01dcdf50e26dd1ffec0378224343892
BorisKlco <klco@pm.me>
2023-09-05 11:05:55 +0200

    2 files modified, 2 added, 2 deleted

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-import './style.css';
-
 document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>js.BrowserGames</h1>
-    <p class="read-the-docs">
-      Text
-    </p>
-    <a href="/001-stopwatch/index.html">StopWatch - class excersie</a>
+  <div class="flex flex-col gap-12 items-center">
+    <h1 class="text-6xl font-bold">js.BrowserGames</h1>
+    <div class="flex flex-col gap-4 items-center">
+    <a class="text-blue-600 hover:text-blue-400 font-semibold" href="./000-stopwatch/index.html">StopWatch - class exercise</a>
+    <a class="text-blue-600 hover:text-blue-400 font-semibold" href="./001-sprite-animation/index.html">Sprite Animation</a>
+    </div>
   </div>
 `;
