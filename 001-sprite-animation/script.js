22d4b0a64d48e53dd678613fb9aaa163ca297de8
BorisKlco <klco@pm.me>
2023-09-05 13:55:23 +0200

    1 binaries uploaded

--- 001-sprite-animation/script.js
+++ 001-sprite-animation/script.js
@@ -1,9 +1,5 @@
-// Function to update the content of the div
-function updateDiv() {
-  const divElement = document.getElementById('myDiv');
-  // Update the content of the div with a new value (e.g., a timestamp)
-  divElement.textContent = new Date().toLocaleTimeString();
-}
+const canvas = document.getElementById('box');
+const ctx = canvas.getAttribute('2d');
 
-// Set up an interval to call the updateDiv function every 10ms
-setInterval(updateDiv, 10);
+const C_WIDTH = (canvas.width = 600);
+const C_HEIGHT = (canvas.height = 600);
