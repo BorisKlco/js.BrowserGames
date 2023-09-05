c537d78457a6661f44d7ce5138e3cdc847999f79
BorisKlco <klco@pm.me>
2023-09-05 14:48:31 +0200

    1 file modified

--- 001-sprite-animation/script.js
+++ 001-sprite-animation/script.js
@@ -3,14 +3,32 @@ const ctx = canvas.getContext('2d');
 
 const C_WIDTH = (canvas.width = 600);
 const C_HEIGHT = (canvas.height = 600);
+const S_WIDTH = 6876 / 12 + 2;
+const S_HEIGHT = 5230 / 10 + 2;
 
 const animationFrames = new Image();
 animationFrames.src = 'anim.png';
+let x = 0;
+let y = 0;
 
-function exampleBox() {
+function animate() {
   ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
-  ctx.fillRect(50, 50, 100, 100);
-  requestAnimationFrame(exampleBox);
+  ctx.drawImage(
+    animationFrames,
+    x * S_WIDTH,
+    y * S_HEIGHT,
+    S_WIDTH,
+    S_HEIGHT,
+    0,
+    0,
+    S_WIDTH,
+    S_HEIGHT
+  );
+  x++;
+  if (x > 6) {
+    x = 0;
+  }
+  requestAnimationFrame(animate);
 }
 
-exampleBox();
+animate();
