7f3a5235ce2a0c189966f9e158d7039856ab8c20
BorisKlco <klco@pm.me>
2023-09-06 11:11:29 +0200

    1 file modified

--- 001-sprite-animation/script.js
+++ 001-sprite-animation/script.js
@@ -1,17 +1,108 @@
 const canvas = document.getElementById('box');
 const ctx = canvas.getContext('2d');
 
-const C_WIDTH = (canvas.width = 600);
-const C_HEIGHT = (canvas.height = 600);
 const S_WIDTH = 6876 / 12 + 2;
 const S_HEIGHT = 5230 / 10 + 2;
 
 const animationFrames = new Image();
 animationFrames.src = 'anim.png';
-let x = 0;
-let y = 0;
-let gameSpeed = 0;
-const gameSlowingRatio = 3;
+
+const animate = [
+  {
+    type: 'idle',
+    frames: 7,
+  },
+  {
+    type: 'jump',
+    frames: 7,
+  },
+  {
+    type: 'fall',
+    frames: 7,
+  },
+  {
+    type: 'run',
+    frames: 9,
+  },
+  {
+    type: 'dizzy',
+    frames: 9,
+  },
+  {
+    type: 'sit',
+    frames: 5,
+  },
+  {
+    type: 'roll',
+    frames: 7,
+  },
+  {
+    type: 'bite',
+    frames: 7,
+  },
+  {
+    type: 'ko',
+    frames: 12,
+  },
+  {
+    type: 'gethit',
+    frames: 4,
+  },
+];
+
+class Game {
+  constructor(
+    cWidth,
+    cHeight,
+    image,
+    frameWidth,
+    frameHeight,
+    margin,
+    animate
+  ) {
+    this.cWidth = canvas.width = cWidth;
+    this.cHeight = canvas.height = cHeight;
+    this.animationImage = new Image();
+    this.animationImage.src = image;
+    this.image = image;
+    this.frameWidth = frameWidth + margin;
+    this.frameHeight = frameHeight + margin;
+    this.gameSpeed = 3;
+    this.animate = animate;
+    this.animation = [];
+
+    this.animate.forEach((item, index) => {
+      let animationFrame = {
+        type: item.type,
+        frames: item.frames,
+        frame: [],
+      };
+
+      for (let i = 0; i < item.frames; i++) {
+        let x = i * this.frameWidth;
+        let y = index * this.frameHeight;
+        animationFrame.frame.push({ x: x, y: y });
+      }
+      this.animation.push(animationFrame);
+    });
+  }
+
+  getFrames() {
+    return this.animation;
+  }
+
+  getStats() {
+    console.log(`SourceImage: ${this.animationImage.src}
+    \n ImageResolution: ${this.frameWidth} x ${this.frameHeight}
+    \n CanvasResolution: ${this.cWidth} x ${this.cHeight}
+    \n GameSpeed: ${this.gameSpeed}`);
+  }
+
+  setSpeed(speed) {
+    this.gameSpeed = speed;
+    return `Game speed changed to ${this.gameSpeed}`;
+  }
+}
 
 // function animate() {
 //   ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
@@ -38,23 +129,23 @@ const gameSlowingRatio = 3;
 
 // animate();
 
-function animate() {
-  ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
-  ctx.drawImage(
-    animationFrames,
-    x * S_WIDTH,
-    y * S_HEIGHT,
-    S_WIDTH,
-    S_HEIGHT,
-    0,
-    0,
-    S_WIDTH,
-    S_HEIGHT
-  );
-  x++;
-  if (x > 6) {
-    x = 0;
-  }
-}
+// function animate() {
+//   ctx.clearRect(0, 0, C_WIDTH, C_HEIGHT);
+//   ctx.drawImage(
+//     animationFrames,
+//     x * S_WIDTH,
+//     y * S_HEIGHT,
+//     S_WIDTH,
+//     S_HEIGHT,
+//     0,
+//     0,
+//     S_WIDTH,
+//     S_HEIGHT
+//   );
+//   x++;
+//   if (x > 6) {
+//     x = 0;
+//   }
+// }
 
-setInterval(() => requestAnimationFrame(animate), 40);
+// setInterval(() => requestAnimationFrame(animate), 40);
