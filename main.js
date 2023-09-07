ac0a8b7b4651652ac1cd6acc6e0c27b7dc99b0f8
BorisKlco <klco@pm.me>
2023-09-07 13:34:01 +0200

    1 binaries uploaded

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.querySelector('#app').innerHTML = `
   <div class="flex flex-col gap-12 items-center">
     <h1 class="text-5xl font-bold">js.BrowserGames 🎮</h1>
     <div class="flex flex-col gap-2 items-left">
+    <a class="text-blue-600 hover:text-blue-400 font-semibold" href="./002-parallax/index.html">002 - Parallax backgrounds</a>
     <a class="text-blue-600 hover:text-blue-400 font-semibold" href="./001-sprite-animation/index.html">001 - Sprite Animation</a>
     <a class="text-blue-600 hover:text-blue-400 font-semibold" href="./000-stopwatch/index.html">000 - StopWatch - class exercise</a>
     </div>
