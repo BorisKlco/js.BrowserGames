4ea4d005febaaf60d5514a44135421bf6c9f845c
BorisKlco <klco@pm.me>
2023-09-09 10:40:26 +0200

    1 file modified

--- 003-movement/script.js
+++ 003-movement/script.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 
 const CW = (canvas.width = 800);
 const CH = (canvas.height = 480);
+const enemies = [];
 
 class Enemy {
   constructor(image, slides) {
@@ -12,12 +13,12 @@ class Enemy {
     this.image.src = image;
     this.slides = slides;
     this.frameCounter = 0;
-    this.gameSpeed = Math.floor(Math.random() * slides);
+    this.gameSpeed = Math.floor(Math.random() * slides + 1);
     this.w = 0;
     this.h = 0;
     this.positionX = 0;
     this.positionY = 0;
-    this.size = Math.random() * 0.5 + 0.5;
+    this.size = Math.random() * 0.3 + 0.3;
 
     this.image.onload = () => {
       (this.w = Math.floor(this.image.width / this.slides)),
@@ -36,12 +37,13 @@ class Enemy {
   }
 
   updateAnimation() {
-    console.log(this.positionX, this.positionY);
     if (this.x < this.w * (this.slides - 1)) {
       this.frameCounter > this.gameSpeed
         ? (this.frameCounter = 0)
         : this.frameCounter++;
       this.frameCounter % this.slides == 0 ? (this.x += this.w) : null;
+      this.positionX += Math.floor(Math.random() * 3 - 1);
+      this.positionY += Math.floor(Math.random() * 3 - 1);
     } else {
       this.x = 0;
     }
@@ -62,12 +64,16 @@ class Enemy {
   }
 }
 
-const e = new Enemy('enemy1.png', 6);
+for (let i = 0; i < 4; i++) {
+  enemies.push(new Enemy('enemy1.png', 6));
+}
 
 const render = () => {
   ctx.clearRect(0, 0, CW, CH);
-  e.updateAnimation();
-  e.draw();
+  enemies.map((e) => {
+    e.updateAnimation();
+    e.draw();
+  });
   requestAnimationFrame(render);
 };
 
