596b1a70fb40e520cc2da73f373d37fed76bd740
BorisKlco <klco@pm.me>
2023-09-11 20:58:31 +0200

    1 file modified

--- 004-collision/script.js
+++ 004-collision/script.js
@@ -4,23 +4,25 @@ const ctx = canvas.getContext('2d');
 const CW = (canvas.width = 500);
 const CH = (canvas.height = 700);
 const src = 'https://www.frankslaboratory.co.uk/downloads/boom.png';
-const collisions = [];
+const models = [];
 
-class Explosions {
+class Explosion {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
     this.sw = 200;
     this.sh = 179;
-    this.w = sw * 0.5;
-    this.h = sw * 0.5;
+    this.w = this.sw * 0.5;
+    this.h = this.sw * 0.5;
+    this.x = x - this.w * 0.5;
+    this.y = y - this.h * 0.5;
     this.img = new Image();
     this.img.src = src;
     this.frame = 0;
+    this.timer = 0;
   }
 
   update() {
-    this.frame > 5 ? (this.frame = 0) : ++this.frame;
+    ++this.timer;
+    this.timer % 10 === 0 ? ++this.frame : null;
   }
 
   draw() {
@@ -30,15 +32,26 @@ class Explosions {
       0,
       this.sw,
       this.sh,
-      x,
-      y,
+      this.x,
+      this.y,
       this.w,
       this.h
     );
   }
 }
 
-// window.addEventListener('click', (e) => {
-//   console.log(e);
-//   ctx.fillRect(e.layerX - 25, e.layerY - 25, 50, 50);
-// });
+window.addEventListener('click', (e) => {
+  console.log(e);
+  models.push(new Explosion(e.layerX, e.layerY));
+});
+
+function render() {
+  ctx.clearRect(0, 0, CW, CH);
+  models.map((explosion) => {
+    explosion.update();
+    explosion.draw();
+  });
+  requestAnimationFrame(render);
+}
+
+render();
