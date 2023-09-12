9e84b8e7a7c478f29427b4f8d61bd9f101b9f9f9
BorisKlco <klco@pm.me>
2023-09-12 18:16:52 +0200

    1 file modified, 2 added

---
+++ 005-game/script.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector('#box');
+const ctx = canvas.getContext('2d');
+
+const CW = (canvas.width = 500);
+const CH = (canvas.height = 700);
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ document.querySelector('#app').innerHTML = `
   <div class="flex flex-col gap-12 items-center">
     <h1 class="text-5xl font-bold">js.BrowserGames 🎮</h1>
     <div class="flex flex-col gap-2 items-left">
+    <a class="text-blue-600 hover:text-blue-400 font-semibold" href="./005-game/index.html">005 - Game 🔫</a>
     <a class="text-blue-600 hover:text-blue-400 font-semibold" href="./004-collision/index.html">004 - Colision Animation</a>
     <a class="text-blue-600 hover:text-blue-400 font-semibold" href="./003-movement/index.html">003 - Movement Patterns</a>
     <a class="text-blue-600 hover:text-blue-400 font-semibold" href="./002-parallax/index.html">002 - Parallax backgrounds</a>
