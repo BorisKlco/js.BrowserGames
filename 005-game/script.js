4896e3112c29d8865c411becfe12a5c52bb25030
BorisKlco <klco@pm.me>
2023-09-12 18:27:25 +0200

    1 file modified

--- 005-game/script.js
+++ 005-game/script.js
@@ -1,5 +1,45 @@
 const canvas = document.querySelector('#box');
 const ctx = canvas.getContext('2d');
 
-const CW = (canvas.width = 500);
-const CH = (canvas.height = 700);
+const ravenSrc = 'https://www.frankslaboratory.co.uk/downloads/raven.png';
+const clickSrc = 'https://www.frankslaboratory.co.uk/downloads/boom.png';
+
+const CW = (canvas.width = 800);
+const CH = (canvas.height = 600);
+
+const enemies = [];
+
+class Enemy {
+  constructor() {
+    this.w = 100;
+    this.h = 50;
+    this.x = CW;
+    this.y = Math.random() * (CH - this.h);
+    this.dirx = Math.random() * 5 + 3;
+    this.diry = Math.random() * 5 - 2.5;
+  }
+
+  update() {
+    this.x -= this.dirx;
+  }
+
+  draw() {
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
+}
+
+for (let i = 0; i < 10; ++i) {
+  enemies.push(new Enemy());
+}
+
+console.log(enemies);
+function render() {
+  ctx.clearRect(0, 0, CW, CH);
+  enemies.map((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+  requestAnimationFrame(render);
+}
+
+// render();
